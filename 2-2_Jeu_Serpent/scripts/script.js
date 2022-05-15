5644005271d5ecbccde90b4d69ec0f4a1438ef4d
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-05-15 22:00:33 +0200

    refreshCan

--- 2-2_Jeu_Serpent/scripts/script.js
+++ 2-2_Jeu_Serpent/scripts/script.js
@@ -1,12 +1,37 @@
 window.onload = function () {
-    var canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "1px solid";
-    
-    document.body.appendChild(canvas);
 
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#f00";
-    ctx.fillRect(60, 60, 200, 75);
+    //Variables
+    let canvas;
+    let ctx;
+    let xCoord = 0, yCoord = 0;
+    const delay = 50;
+
+    //Functions
+    function init()
+    {
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+
+        document.body.appendChild(canvas);
+
+        ctx = canvas.getContext('2d');
+        refreshCan();
+    }
+
+    function refreshCan()
+    {
+        xCoord += 2;
+        yCoord += 2;
+
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        ctx.fillStyle = "#f00";
+        ctx.fillRect(xCoord, yCoord, 100, 50);
+        setTimeout(refreshCan, delay);
+    }
+
+    //Main
+    init();
+
 }
