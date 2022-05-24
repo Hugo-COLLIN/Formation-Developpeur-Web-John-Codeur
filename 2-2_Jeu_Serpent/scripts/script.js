51466f6f85064f4f5bb635ee1ed4d12b37816aac
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-05-24 10:25:56 +0200

    Serpent bouge

--- 2-2_Jeu_Serpent/scripts/script.js
+++ 2-2_Jeu_Serpent/scripts/script.js
@@ -4,18 +4,11 @@ window.onload = function () {
     let canvas;
     let ctx;
     let snake;
+    let nextPos;
 
-    let xCoord = 0, yCoord = 0;
     const canWidth = 900, canHeight = 600;
     const blockSize = 30;
-    const delay = 50;
-
-    function drawBlock(ctx, elem)
-    {
-        let x = elem[0] * blockSize;
-        let y = elem[1] * blockSize;
-        ctx.fillRect(x, y, blockSize, blockSize);
-    }
+    const delay = 100;
 
     //Constructors
     function Snake (body)
@@ -25,13 +18,19 @@ window.onload = function () {
         this.draw = function () {
             ctx.save();
             ctx.fillStyle = "#f00";
-            //for (const elem in this.body)
-                //drawBlock(ctx, elem);
-                //console.log(elem[0], elem[1]);
 
             for (let i = 0 ; i < this.body.length ; i ++)
                 drawBlock(ctx, this.body[i]);
             ctx.restore();
+        }
+
+        this.advance = function () {
+            //console.log(this.body[0]);
+            nextPos = this.body[0].slice(); //https://bobbyhadz.com/blog/javascript-typeerror-slice-is-not-a-function
+            nextPos[0] ++;
+            this.body.unshift(nextPos);
+            this.body.pop();
+            //console.log(this.body[0]);
         }
     }
 
@@ -50,9 +49,18 @@ window.onload = function () {
         refreshCan();
     }
 
+    function drawBlock(ctx, elem)
+    {
+        let x = elem[0] * blockSize;
+        let y = elem[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+
     function refreshCan()
     {
+        snake.advance();
         ctx.clearRect(0,0, canvas.width, canvas.height);
+
         snake.draw();
         setTimeout(refreshCan, delay);
     }
