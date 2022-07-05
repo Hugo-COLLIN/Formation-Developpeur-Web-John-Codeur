4fd1c882eef92f2a23eab68c612fcffeeb37a168
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-07-06 00:29:02 +0200

    Top5 animation ouverture/fermeture

--- 3-2_Site_Top5/js/script.js
+++ 3-2_Site_Top5/js/script.js
@@ -1,21 +1,46 @@
 $(function () {
-    const   $menuItems = $("main ul").children("li"), /*Les variables qui commencent par $ contiennent du JQuery, pas obligatoire.*/
-            totalMenuItems = $menuItems.length;
-    let     openedIndex = -1;
+    const SPEED = 250;
 
-    let init = function () {
+    const $menuItems = $("main ul").children("li"), /*Les variables qui commencent par $ contiennent du JQuery, pas obligatoire.*/
+          totalMenuItems = $menuItems.length;
+    let   openedIndex = -1;
+
+    const init = function ()
+    {
         $menuItems.children(".images").click(function () {
-            const   newIndex = $(this).parent().index(),
-                    $item = $menuItems.eq(newIndex),
-                    $colorImage = $item.find(".color");
+            const newIndex = $(this).parent().index(),
+                  $item = $menuItems.eq(newIndex);
 
-            $colorImage.animate({left:"0"}, 250);
-            $item.animate({width:"420px"}, 250);
-
-            openedIndex = newIndex;
+            if (validIndex(newIndex))
+                if (openedIndex === newIndex)
+                {
+                    animateItem($item, false, SPEED);
+                    openedIndex = -1;
+                }
+                else
+                {
+                    animateItem($menuItems.eq(openedIndex), false, SPEED)
+                    animateItem($item, true, SPEED);
+                    openedIndex = newIndex;
+                }
 
         })
     };
+
+    const validIndex = function (indexToCheck)
+    {
+        return (indexToCheck >= 0) && (indexToCheck < totalMenuItems)
+    }
+
+    const animateItem = function ($item, isToOpen, speed)
+    {
+        const $colorImage = $item.find(".color"),
+              itemParam = isToOpen ? {width: "420px"} : {width: "140px"},
+              colorImageParam = isToOpen ? {left: "0"} : {left: "140px"};
+
+        $item.animate(itemParam, speed);
+        $colorImage.animate(colorImageParam, speed);
+    }
 
     init();
 
